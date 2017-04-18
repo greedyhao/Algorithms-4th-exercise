5c5884c60c39de0ffdb6df3a287d280c307f6ada
JuHao <hao_kr@163.com>
2017-04-18 14:39:46 +0800

    Create 1.1.20.py

---
+++ exercise 1/1.1.20.py
@@ -0,0 +1,25 @@
+def ln(N):
+	lo = 0
+	hi = N
+	while lo < hi:
+		mid = (lo+hi)/2
+		if mid**2 < N:
+			lo = mid
+		if mid**2 > N:
+			hi = mid
+		if abs(mid**2 - N) < 0.01:
+			return mid
+	return 'Error'
+
+def recursive(N):
+    return recursive1(N,1)
+
+def recursive1(N, result):
+	if N == 1:
+		return result
+	if N > 1:
+		result *= N
+		return recursive1(N-1, result)
+
+N = (float)(input())
+print (ln(recursive(N)))
