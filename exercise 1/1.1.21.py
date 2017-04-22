0b85bf20f4e986ee1082e7020e5538f8f5af9a11
JuHao <hao_kr@163.com>
2017-04-23 00:10:19 +0800

    Update 1.1.21.py

--- exercise 1/1.1.21.py
+++ exercise 1/1.1.21.py
@@ -1,8 +1,6 @@
-def main():
+if __name-- == "__main__":
     info = input()
     info_split = info.split()
     num = round((float)(info_split[1])/(float)(info_split[2]),3)
     info_split.append(num)
     print(info_split)
-
-main()
